f0d49276301aa9b4e7f81c8b10e0f56b2c0626a7
longth <longth1@vmodev.com>
2024-09-04 15:19:39 +0700

    [LongTH]: Update logic redirect url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
-import Banner from '@/components/Banner'
-import Navbar from '@/components/Navbar'
 import SliderComponent from '@/components/SliderComponent'
 import News from '@/components/News'
-import NavbarFooter from '@/components/NavbarFooter'
-import Footer from '@/components/Footer'
 
 function Home() {
   return (

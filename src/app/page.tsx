90c0260403c88171f91d696e85fc37c695610ee0
longth <longth1@vmodev.com>
2024-11-05 08:51:41 +0700

    [LongTH]: Add video youtube

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,9 @@ function Home() {
     <main className='min-h-screen'>
       <SliderComponent />
         <News />
+      <div className="flex justify-center mt-8">
+        <iframe width="1024" height="650" src="https://www.youtube.com/embed/tTu907I0JPc?si=JCsPE65wPkKQ4VkD" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+      </div>
     </main>
   )
 }

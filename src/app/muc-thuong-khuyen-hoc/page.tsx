c4bf1c952b5a6a57a43c24b46272cb3f140b0671
longth <longthse04936@gmail.com>
2025-09-13 09:55:37 +0700

    [LongTH]: Update font chữ

--- src/app/muc-thuong-khuyen-hoc/page.tsx
+++ src/app/muc-thuong-khuyen-hoc/page.tsx
@@ -7,7 +7,7 @@ const Page = async () => {
   // Sử dụng fs để đọc file
   const htmlContent = fs.readFileSync(filePath, 'utf8');
   return (
-    <div className='w-full max-w-[700px] px-4 sm:px-6 mx-auto my-6'>
+    <div className='w-full max-w-[800px] px-4 sm:px-6 mx-auto my-6'>
       <h1 className='text-2xl md:text-3xl text-center font-semibold mb-6'>Mức thưởng khuyến học</h1>
       <div
         className='font-sans bg-slate-100 px-4 sm:px-6 py-4 rounded-md shadow-sm'
